2f2cc38995754cea55ee98385740b38b56e76004
Erindcl <zhifei@dtstack.com>
2019-09-19 15:50:20 +0800

    update useFetch

--- component/useFetch/useFetch.tsx
+++ component/useFetch/useFetch.tsx
@@ -1,39 +1,31 @@
-import { useEffect, useRef, useState, useCallback } from 'react'
-import { isEqual } from 'lodash'
-import {message} from 'antd'
-import {API} from '@/api'
-const useFetch= (method,params,initRes) => {
-  const prevParams = useRef(null);
-  const [callback,{ loading, res }] = useFetchCallback(method,initRes)
-  useEffect(() => {
-    if (!isEqual(prevParams.current, params)) {
-      prevParams.current = params
-      callback(params)
-    }
-  })
+import { useState, useEffect, useCallback } from 'react';
+import { message as Message } from 'antd'
+import { API } from '@/api'
 
-  return { loading, res }
-}
- const useFetchCallback:any= (method,initRes) => {
-  const [loading, setLoading] = useState(false)
-  const [res, setRes] = useState(initRes);
-  const callback = useCallback(
-    params => {
-      setLoading(true);
-      API[method](params)
-        .then(ret => {
-          setLoading(false)
-          setRes(ret)
-        })
-        .catch(err => {
-          setLoading(false)
-          message.error(`请求出错:${err}`);
+function useFetch(method, params, initRes) {
+    const [loading, setLoading] = useState(false);
+    const [retData, setRetData] = useState(initRes);
+    const callback = useCallback((params) => {
+        setLoading(true);
+        API[method](params).then((ret) => {
+            const { success, data, message } = ret;
+            if (success) {
+                setRetData(data);
+            } else {
+                Message.error(message);
+            }
+            setLoading(false);
+        }).catch((err) => {
+            setLoading(false)
+            Message.error('请求出错：', err);
         })
     },
-    [method]
-  )
+        [method]);
+    useEffect(() => {
+        callback(params);
+    }, [params])
 
-  return [callback, { loading,res }]
+    return { loading, retData }
 }
 
 export default useFetch
